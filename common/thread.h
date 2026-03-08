f5ee27470717a6f60f7470b48bf2496c3558aa80
vibhav950 <vibhavtiwari.official@gmail.com>
2026-03-09 04:24:46 +0530

    Add mutex functions to threads API; CMake changes
    - CMake will now detect platform-specific thread backend
    - Modularize CMakeLists.txt

--- common/thread.h
+++ common/thread.h
@@ -6,9 +6,18 @@
 #if defined(HAVE_POSIX_THREADS)
 #include <pthread.h>
 #define zt_thread_t pthread_t
+#define zt_mutex_t pthread_mutex_t
+#define zt_rwlock_t pthread_rwlock_t
+#define zt_once_t pthread_once_t
+
 #define zt_thread_t_null NULL
 #else
-typedef struct _zt_thread_noapi_st *zt_thread_t;
+struct thread_noapi_st {};
+typedef struct thread_noapi_st zt_thread_t;
+typedef struct thread_noapi_st zt_mutex_t;
+typedef struct thread_noapi_st zt_rwlock_t;
+typedef struct thread_noapi_st zt_once_t;
+
 #define zt_thread_t_null NULL
 #endif
 
@@ -23,5 +32,15 @@ err_t zt_thread_setaffinity(zt_thread_t *t, char *cpumask, size_t mask_size);
 zt_thread_t zt_thread_self(void);
 
 int zt_thread_equal(zt_thread_t *t1, zt_thread_t *t2);
+
+err_t zt_mutex_init(zt_mutex_t *mtx);
+
+err_t zt_mutex_init_recursive(zt_mutex_t *mtx);
+
+void zt_mutex_destroy(zt_mutex_t *mtx);
+
+void zt_mutex_lock(zt_mutex_t *mtx);
+
+void zt_mutex_unlock(zt_mutex_t *mtx);
 
 #endif /* __THREAD_H__ */
